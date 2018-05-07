a0227449618fca56ce18872f719780dc23a2ffd2
sarthak <sarthakn@gmail.com>
2018-05-07 18:25:49 -0400

    Removed test code

--- src/dispersibility_client.js
+++ src/dispersibility_client.js
@@ -1,5 +1,3 @@
-import {gpClient} from 'gigapaxos-client';
-
 const EXECUTE_TYPE = 2000;
 
 export function initialize(newReconfiguratorAddress) {
@@ -8,13 +6,4 @@ export function initialize(newReconfiguratorAddress) {
 
 export function sendRequest(service, request, callback) {
   gpClient.sendAppRequest(service, EXECUTE_TYPE, {APP_REQUEST: request}, callback);
-}
-
-export function test() {
-
-  gpClient.initialize('http://127.0.0.1:9300');
-  gpClient.sendAppRequest('LinWritesLocReadsApp0', 402, {REQUEST_VALUE : 0, STOP : false, EPOCH : 0}, function (response) {
-    console.log('Received response : ' + JSON.stringify(response));
-    alert('Received response : ' + JSON.stringify(response))
-  });
 }
